ece3f43377611a3c979de017d1f27acdb6f81d45
developer1@joyra.in <developer1@joyra.in>
2021-07-20 16:31:28 +0530

    update

---
+++ ShopNow/Controllers/ProductTypeController.cs
@@ -0,0 +1,27 @@
+﻿using ShopNow.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopNow.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private ShopnowchatEntities db = new ShopnowchatEntities();
+
+        public async Task<JsonResult> GetSelect2List(string q = "")
+        {
+            var model = await db.ProductTypes.OrderBy(i => i.Name).Where(a => a.Name.Contains(q)).Select(i => new
+            {
+                id = i.Id,
+                text = i.Name
+            }).OrderBy(i=>i.id).ToListAsync();
+
+            return Json(new { results = model, pagination = new { more = false } }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
