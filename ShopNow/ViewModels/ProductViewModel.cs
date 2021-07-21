3f6cad09a9e4586e38c5047183e2f05387814b1c
developer1@joyra.in <developer1@joyra.in>
2021-07-21 16:33:34 +0530

    update

--- ShopNow/ViewModels/ProductViewModel.cs
+++ ShopNow/ViewModels/ProductViewModel.cs
@@ -26,229 +26,6 @@ namespace ShopNow.ViewModels
         }
     }
 
-    public class ProductCreateViewModel
-    {
-        public HttpPostedFileBase ProductImage1 { get; set; }
-        public HttpPostedFileBase ProductImage2 { get; set; }
-        public HttpPostedFileBase ProductImage3 { get; set; }
-        public HttpPostedFileBase ProductImage4 { get; set; }
-        public HttpPostedFileBase ProductImage5 { get; set; }
-        public string Name { get; set; }
-        public int MasterProductId { get; set; }
-        public string MasterProductName { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
-        public string Specification { get; set; }
-        public int ShopId { get; set; }
-        public string ShopName { get; set; }
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public int BrandId { get; set; }
-        public string BrandName { get; set; }
-        public string ProductType { get; set; }
-        public int ShopCategoryId { get; set; }
-        public string ShopCategoryName { get; set; }
-        public string MainSNCode { get; set; }
-        public string GTIN { get; set; }
-        public string UPC { get; set; }
-        public string GTIN14 { get; set; }
-        public string EAN { get; set; }
-        public string ISBN { get; set; }
-        public string ImagePath { get; set; }
-        public double Price { get; set; }
-        public double MenuPrice { get; set; }
-        public int Qty { get; set; }
-        public int Percentage { get; set; }
-        public int Status { get; set; }
-        public bool Customisation { get; set; }
-        public string CreatedBy { get; set; }
-        public string UpdatedBy { get; set; }
-        public string Product { get; set; }
-        public string Entry { get; set; }
-        public string GoogleTaxonomyCode { get; set; }
-        public string ImagePathLarge1 { get; set; }
-        public string ImagePathLarge2 { get; set; }
-        public string ImagePathLarge3 { get; set; }
-        public string ImagePathLarge4 { get; set; }
-        public string ImagePathLarge5 { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
-
-        // Default Medical Product
-        public string MasterProductCode1 { get; set; }
-        public string MasterProductName1 { get; set; }
-        public string CategoryCode1 { get; set; }
-        public string CategoryName1 { get; set; }
-        public string BrandCode1 { get; set; }
-        public string BrandName1 { get; set; }
-        public double Price1 { get; set; }
-        public double MenuPrice1 { get; set; }
-        public int Qty1 { get; set; }
-        public int Percentage1 { get; set; }
-        public string DrugMeasurementUnitCode { get; set; }
-        public string DrugMeasurementUnitName { get; set; }
-        public bool PriscriptionCategory { get; set; }
-        public string[] DrugCompoundDetailCode { get; set; }
-        public string CombinationDrugCompound { get; set; }
-        public string[] DCDCode { get; set; }
-        public string iBarU { get; set; }
-        public string Manufacturer { get; set; }
-        public string OriginCountry { get; set; }
-        public double weight { get; set; }
-        public string SizeLB { get; set; }
-        public string DiscountCategoryCode { get; set; }
-        public string DiscountCategoryName { get; set; }
-        public double DiscountCategoryPercentage { get; set; }
-        public string GoogleTaxonomyCode1 { get; set; }
-
-        // Manual Medical Product
-        public string MasterProductCode2 { get; set; }
-        public string MasterProductName2 { get; set; }
-        public string CategoryCode2 { get; set; }
-        public string CategoryName2 { get; set; }
-        public string BrandCode2 { get; set; }
-        public string BrandName2 { get; set; }
-        public double Price2 { get; set; }
-        public double MenuPrice2 { get; set; }
-        public int Qty2 { get; set; }
-        public int Percentage2 { get; set; }
-        public string DrugMeasurementUnitCode1 { get; set; }
-        public string DrugMeasurementUnitName1 { get; set; }
-        public bool PriscriptionCategory1 { get; set; }
-        public string[] DrugCompoundDetailCode1 { get; set; }
-        public string CombinationDrugCompound1 { get; set; }
-        public string iBarU1 { get; set; }
-        public string Manufacturer1 { get; set; }
-        public string OriginCountry1 { get; set; }
-        public double weight1 { get; set; }
-        public string SizeLB1 { get; set; }
-        public string DiscountCategoryCode1 { get; set; }
-        public string DiscountCategoryName1 { get; set; }
-        public double DiscountCategoryPercentage1 { get; set; }
-        public string GoogleTaxonomyCode2 { get; set; }
-
-        public List<MedicalStockList> MedicalStockLists { get; set; }
-        public class MedicalStockList
-        {
-             
-            public string ProductCode { get; set; }
-            public string ProductName { get; set; }
-            public double Stock { get; set; }
-            public string SupplierName { get; set; }
-            public double MRP { get; set; }
-            public double SalePrice { get; set; }
-            public double TaxPercentage { get; set; }
-            public double DiscountPercentage { get; set; }
-            public double LoyaltyPointsper100Value { get; set; }
-            public double MinimumLoyaltyReducationPercentage { get; set; }
-            public double SpecialCostOfDelivery { get; set; }
-            public int OutLetId { get; set; }
-            public double SpecialPrice { get; set; }
-            public int MinSaleQty { get; set; }
-        }
-    }
-
-    public class ProductEditViewModel
-    {
-        public HttpPostedFileBase ProductImage1 { get; set; }
-        public HttpPostedFileBase ProductImage2 { get; set; }
-        public HttpPostedFileBase ProductImage3 { get; set; }
-        public HttpPostedFileBase ProductImage4 { get; set; }
-        public HttpPostedFileBase ProductImage5 { get; set; }
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int MasterProductId { get; set; }
-        public string MasterProductName { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
-        public string Specification { get; set; }
-        public int ShopId { get; set; }
-        public string ShopName { get; set; }
-        public int CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public int BrandId { get; set; }
-        public string BrandName { get; set; }
-        public string ProductType { get; set; }
-        public int ShopCategoryId { get; set; }
-        public string ShopCategoryName { get; set; }
-        public string MainSNCode { get; set; }
-        public string GTIN { get; set; }
-        public string UPC { get; set; }
-        public string GTIN12 { get; set; }
-        public string GTIN13 { get; set; }
-        public string GTIN14 { get; set; }
-        public string EAN { get; set; }
-        public string ISBN { get; set; }
-        public string ImagePath { get; set; }
-        public double Price { get; set; }
-        public double MenuPrice { get; set; }
-        public int Qty { get; set; }
-        public int Percentage { get; set; }
-        public int Status { get; set; }
-        public DateTime DateEncoded { get; set; }
-        public DateTime DateUpdated { get; set; }
-        public bool Customisation { get; set; }
-        public string GoogleTaxonomyCode { get; set; }
-        public string ImagePathLarge1 { get; set; }
-        public string ImagePathLarge2 { get; set; }
-        public string ImagePathLarge3 { get; set; }
-        public string ImagePathLarge4 { get; set; }
-        public string ImagePathLarge5 { get; set; }
-
-        public List<SpecificationList> SpecificationLists { get; set; }
-        public class SpecificationList
-        {
-            public int Id { get; set; }
-            public int ProductId { get; set; }
-            public string ProductName { get; set; }
-            public int ShopId { get; set; }
-            public string ShopName { get; set; }
-            public int MasterProductId { get; set; }
-            public string MasterProductName { get; set; }
-            public int SpecificationId { get; set; }
-            public string SpecificationName { get; set; }
-            public string Value { get; set; }
-        }
-
-        // Medical Product
-        public string DrugMeasurementUnitCode { get; set; }
-        public string DrugMeasurementUnitName { get; set; }
-        public bool PriscriptionCategory { get; set; }
-        public string[] DrugCompoundDetailCode { get; set; }
-        public string DrugCompoundDetailCode1 { get; set; }
-        public string CombinationDrugCompound { get; set; }
-        public string iBarU { get; set; }
-        public string Manufacturer { get; set; }
-        public string OriginCountry { get; set; }
-        public double weight { get; set; }
-        public string SizeLB { get; set; }
-        public string DiscountCategoryCode { get; set; }
-        public string DiscountCategoryName { get; set; }
-        public double DiscountCategoryPercentage { get; set; }
-        public int DiscountType { get; set; }
-        public int DiscountCategoryType { get; set; }
-
-        public List<MedicalStockList> MedicalStockLists { get; set; }
-        public class MedicalStockList
-        {
-             
-            public string ProductCode { get; set; }
-            public string ProductName { get; set; }
-            public double Stock { get; set; }
-            public string SupplierName { get; set; }
-            public double MRP { get; set; }
-            public double SalePrice { get; set; }
-            public double TaxPercentage { get; set; }
-            public double DiscountPercentage { get; set; }
-            public double LoyaltyPointsper100Value { get; set; }
-            public double MinimumLoyaltyReducationPercentage { get; set; }
-            public double SpecialCostOfDelivery { get; set; }
-            public int OutLetId { get; set; }
-            public double SpecialPrice { get; set; }
-            public int MinSaleQty { get; set; }
-        }
-    }
-
     public class MedicalCreateViewModel
     {
          
@@ -257,12 +34,10 @@ namespace ShopNow.ViewModels
         public string MasterProductName { get; set; }
         public int ShopId { get; set; }
         public string ShopName { get; set; }
-        public int CategoryId { get; set; }
+        public string CategoryIds { get; set; }
         public string CategoryName { get; set; }
-        public string BrandId { get; set; }
+        public int BrandId { get; set; }
         public string BrandName { get; set; }
-        public int ProductTypeId { get; set; }
-        public string ProductTypeName { get; set; }
         public double Price { get; set; }
         public double MenuPrice { get; set; }
         public int Qty { get; set; }
@@ -290,7 +65,9 @@ namespace ShopNow.ViewModels
         public double DiscountCategoryPercentage { get; set; }
         public int DiscountType { get; set; }
         public int DiscountCategoryType { get; set; }
-       
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
+
         public List<MedicalStockList> MedicalStockLists { get; set; }
         public class MedicalStockList
         {
@@ -319,7 +96,7 @@ namespace ShopNow.ViewModels
         public string MasterProductName { get; set; }
         public int ShopId { get; set; }
         public string ShopName { get; set; }
-        public int CategoryId { get; set; }
+        public string CategoryIds { get; set; }
         public string CategoryName { get; set; }
         public int BrandId { get; set; }
         public string BrandName { get; set; }
@@ -350,8 +127,8 @@ namespace ShopNow.ViewModels
         public double DiscountCategoryPercentage { get; set; }
         public int DiscountType { get; set; }
         public int DiscountCategoryType { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
 
         public List<MedicalStockList> MedicalStockLists { get; set; }
         public class MedicalStockList
@@ -658,17 +435,15 @@ namespace ShopNow.ViewModels
         public string MasterProductName { get; set; }
         public int ShopId { get; set; }
         public string ShopName { get; set; }
-        public string ShopCategoryCode { get; set; }
-        public string ShopCategoryName { get; set; }
-        public int CategoryId { get; set; }
+        public string CategoryIds { get; set; }
         public string CategoryName { get; set; }
-        public string SubCategoryCode { get; set; }
+        public string SubCategoryIds { get; set; }
         public string SubCategoryName { get; set; }
-        public string NextSubCategoryCode { get; set; }
+        public string NextSubCategoryIds { get; set; }
         public string NextSubCategoryName { get; set; }
-        public string PackageCode { get; set; }
+        public int PackageId { get; set; }
         public string PackageName { get; set; }
-        public string MeasurementUnitCode { get; set; }
+        public int MeasurementUnitId { get; set; }
         public string MeasurementUnitName { get; set; }
         public string GoogleTaxonomyCode { get; set; }
         public int BrandId { get; set; }
@@ -683,21 +458,14 @@ namespace ShopNow.ViewModels
         public string ImagePathLarge5 { get; set; }
         public double Price { get; set; }
         public double MenuPrice { get; set; }
-        public string ASIN { get; set; }
-        public double weight { get; set; }
+        public double Weight { get; set; }
         public string SizeLB { get; set; }
-        public string ProductType { get; set; }
-        public string CreatedBy { get; set; }
-        public string UpdatedBy { get; set; }
-        public int Status { get; set; }
-        public string iBarU { get; set; }
-        public string OriginCountry { get; set; }
-        public string Manufacturer { get; set; }
-        public Nullable<int> shopid { get; set; }
+        public int IBarU { get; set; }
+        public int Shopid { get; set; }
         public int Qty { get; set; }
         public int Percentage { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
 
     }
 
@@ -742,74 +510,54 @@ namespace ShopNow.ViewModels
         public Nullable<int> shopid { get; set; }
         public int Qty { get; set; }
         public int Percentage { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
 
     }
     public class FoodCreateViewModel
     {
-         
         public string Name { get; set; }
-        public string NickName { get; set; }
         public int MasterProductId { get; set; }
         public string MasterProductName { get; set; }
         public int ShopId { get; set; }
         public string ShopName { get; set; }
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string ShopCategoryCode { get; set; }
-        public string ShopCategoryName { get; set; }
-        public string ProductType { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
         public int MinSelectionLimit { get; set; }
         public int MaxSelectionLimit { get; set; }
         public string ColorCode { get; set; }
-        public string ImagePath { get; set; }
         public bool Customisation { get; set; }
         public double Price { get; set; }
         public double MenuPrice { get; set; }
         public int Qty { get; set; }
         public int Percentage { get; set; }
-        public int Status { get; set; }
-        public string Entry { get; set; }
         public string GoogleTaxonomyCode { get; set; }
         public string ImagePathLarge1 { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
     }
     public class FoodEditViewModel
     {
         public int Id { get; set; }
-         
         public string Name { get; set; }
-        public string NickName { get; set; }
         public int MasterProductId { get; set; }
         public string MasterProductName { get; set; }
         public int ShopId { get; set; }
         public string ShopName { get; set; }
-        public int CategoryId { get; set; }
+        public string CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string ShopCategoryCode { get; set; }
-        public string ShopCategoryName { get; set; }
-        public string ProductType { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
         public int MinSelectionLimit { get; set; }
         public int MaxSelectionLimit { get; set; }
         public string ColorCode { get; set; }
-        public string ImagePath { get; set; }
         public bool Customisation { get; set; }
         public double Price { get; set; }
         public double MenuPrice { get; set; }
         public int Qty { get; set; }
         public int Percentage { get; set; }
-        public int Status { get; set; }
-        public string Entry { get; set; }
         public string GoogleTaxonomyCode { get; set; }
         public string ImagePathLarge1 { get; set; }
-        public Nullable<int> PackagingType { get; set; }
-        public Nullable<double> PackagingCharge { get; set; }
+        public int PackingType { get; set; }
+        public double PackingCharge { get; set; }
     }
 
     public class ShopAddOnSessionAddViewModel
