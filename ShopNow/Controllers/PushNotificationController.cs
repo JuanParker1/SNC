2538a3458cc22153242bca1f84108c52a178c4b0
developer1@joyra.in <developer1@joyra.in>
2021-07-31 17:18:10 +0530

    update

--- ShopNow/Controllers/PushNotificationController.cs
+++ ShopNow/Controllers/PushNotificationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShopNow.Filters;
+using ShopNow.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,20 @@ namespace ShopNow.Controllers
 {
     public class PushNotificationController : Controller
     {
-        
+        private sncEntities db = new sncEntities();
+
+        [AccessPolicy(PageCode = "")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AccessPolicy(PageCode = "")]
         [HttpPost]
-        public ActionResult SendBulk()
+        public ActionResult SendBulk(string title,string message)
         {
+            var fcmTokenList = db.Customers.Where(i => !string.IsNullOrEmpty(i.FcmTocken)).Select(i=>i.FcmTocken).ToArray();
+            Helpers.PushNotification.SendBulk(message, title, "a.mp3", fcmTokenList);
             return RedirectToAction("Index");
         }
     }
