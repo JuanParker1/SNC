3e53c60eea265406eb7ebc06e57b2a204313c260
developer1@joyra.in <developer1@joyra.in>
2021-07-31 15:32:17 +0530

    update

---
+++ ShopNow/Controllers/PushNotificationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopNow.Controllers
+{
+    public class PushNotificationController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendBulk()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
