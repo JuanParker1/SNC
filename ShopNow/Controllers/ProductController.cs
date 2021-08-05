2ec6b226b2486f815cb7cc37c85789b5bc16f088
joyrabox12 <joyrabox12@gmail.com>
2021-08-05 12:27:21 +0530

    update

--- ShopNow/Controllers/ProductController.cs
+++ ShopNow/Controllers/ProductController.cs
@@ -1408,6 +1408,7 @@ namespace ShopNow.Controllers
             var model = new ProductMappingViewModel();
             return View(model);
         }
+
         [HttpPost]
         [AccessPolicy(PageCode = "SHNPROC001")]
         public ActionResult ShopItemMapping(int originalShopId, int newShopId, string newShopName)
