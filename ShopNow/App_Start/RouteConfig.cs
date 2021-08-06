0fcc0f08987805065d4f6b8f0b3984abec0d30d9
developer1@joyra.in <developer1@joyra.in>
2021-08-06 17:28:29 +0530

    update

--- ShopNow/App_Start/RouteConfig.cs
+++ ShopNow/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ShopNow
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Support", action = "LivePending", id = UrlParameter.Optional }
             );
         }
     }
