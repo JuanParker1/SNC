b684d266ebec48331ae457ae1a54ba6e60c7a1be
developer1@joyra.in <developer1@joyra.in>
2021-08-16 12:03:55 +0530

    update

---
+++ ShopNow/Controllers/ApiSettingController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using ShopNow.Filters;
+using ShopNow.Models;
+using ShopNow.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopNow.Controllers
+{
+    public class ApiSettingController : Controller
+    {
+        private sncEntities db = new sncEntities();
+        private IMapper _mapper;
+        private MapperConfiguration _mapperConfiguration;
+
+        public ApiSettingController()
+        {
+
+        }
+
+        [AccessPolicy(PageCode = "")]
+        public ActionResult List()
+        {
+            var user = ((ShopNow.Helpers.Sessions.User)Session["USER"]);
+            ViewBag.Name = user.Name;
+            var model = new ApiSettingListViewModel();
+            model.ListItems = db.ApiSettings.Where(i => i.Status == 0)
+                .Select(i => new ApiSettingListViewModel.ListItem
+                {
+                  
+                    Category = i.Category,
+                    Id = i.Id,
+                    OutletId = i.OutletId,
+                    ProviderName = i.ProviderName,
+                    ShopId = i.ShopId,
+                    ShopName = i.ShopName,
+                    Url = i.Url,
+                    Version = i.Version
+                }).ToList();
+            return View(model);
+        }
+
+        [AccessPolicy(PageCode = "")]
+        public ActionResult Create()
+        {
+            var user = ((ShopNow.Helpers.Sessions.User)Session["USER"]);
+            ViewBag.Name = user.Name;
+            return View();
+        }
+    }
+}
---
+++ ShopNow/ViewModels/ApiSettingViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopNow.ViewModels
+{
+    public class ApiSettingListViewModel
+    {
+        public List<ListItem> ListItems { get; set; }
+        public class ListItem
+        {
+            public int Id { get; set; }
+            public int ShopId { get; set; }
+            public string ShopName { get; set; }
+            public string ProviderName { get; set; }
+            public string Version { get; set; }
+            public string Url { get; set; }
+            public int Category { get; set; }
+            public int OutletId { get; set; }
+        }
+    }
+
+    public class ApiSettingCreateViewModel
+    {
+        public int ShopId { get; set; }
+        public string ShopName { get; set; }
+        public string ProviderName { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+        public string AuthName { get; set; }
+        public string AuthKey { get; set; }
+        public string Remark { get; set; }
+        public int Category { get; set; }
+        public int OutletId { get; set; }
+    }
+}
