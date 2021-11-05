352662925c86a1cc66334ea7781627cefe841498
developer1@joyra.in <developer1@joyra.in>
2021-11-05 12:44:28 +0530

    update

--- ShopNow/Models/ApplicationDbContext.Context.cs
+++ ShopNow/Models/ApplicationDbContext.Context.cs
@@ -93,10 +93,10 @@ namespace ShopNow.Models
         public virtual DbSet<Shop> Shops { get; set; }
         public virtual DbSet<CustomerSearchData> CustomerSearchDatas { get; set; }
         public virtual DbSet<CustomerWalletHistory> CustomerWalletHistories { get; set; }
-        public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<Banner> Banners { get; set; }
         public virtual DbSet<CustomerPrescriptionImage> CustomerPrescriptionImages { get; set; }
         public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
     
         [DbFunction("sncEntities", "GetTableVAlueString")]
         public virtual IQueryable<GetTableVAlueString_Result> GetTableVAlueString(string key)
