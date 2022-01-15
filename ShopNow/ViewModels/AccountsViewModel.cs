9067e68b204fb4d2a9c5e3d444635a4887dfe60e
developer1@joyra.in <developer1@joyra.in>
2022-01-15 17:02:28 +0530

    update

---
+++ ShopNow/ViewModels/AccountsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopNow.ViewModels
+{
+    public class AccountsBillWiseReportViewModel
+    {
+        public List<ListItem> ListItems { get; set; }
+        public class ListItem
+        {
+            public int SNo { get; set; }
+            public DateTime Date { get; set; }
+            public int OrderNumber { get; set; }
+            public string ShopName { get; set; }
+            public string GSTNumber { get; set; }
+            public double MenuPrice { get; set; }
+            public double ShopBillAmount { get; set; }
+            public double PriceDifference { get; set; }
+            public double CustomerPaidAmount { get; set; }
+            public double RefundAmount { get; set; }
+            public double FinalAmount { get; set; }
+            public double DeliveryAmountFromCustomer { get; set; }
+            public double DeliveryDiscount { get; set; }
+            public double TotalDeliveryCharge { get; set; }
+            public double DeliveryChargePaidToDeliveryBoy { get; set; }
+            public double DeliveryChargeProfit { get; set; }
+            public double AmountProfit { get; set; }
+        }
+    }
+}
