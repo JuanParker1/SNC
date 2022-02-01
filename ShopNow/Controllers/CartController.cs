2a5bea401cb902fcc2b4efe894feca1c54513cdd
developer1@joyra.in <developer1@joyra.in>
2022-02-01 10:19:29 +0530

    update

--- ShopNow/Controllers/CartController.cs
+++ ShopNow/Controllers/CartController.cs
@@ -1756,45 +1756,6 @@ namespace ShopNow.Controllers
                 }).ToList();
 
             return Json(ordersList, JsonRequestBehavior.AllowGet);
-
-
-            //var joyraLocation = db.Shops.Where(i => i.Id == 123)
-            //    .Select(i => new 
-            //    {
-            //        Latitude = i.Latitude,
-            //        Longitude = i.Longitude,
-            //        Address = i.Name
-            //    }).ToList();
-
-            //var deliverylocationlist = db.Orders.Where(i => i.Status == 2 || i.Status == 3)
-            //     .AsEnumerable()
-            //     .Select(i => new 
-            //     {
-            //         Latitude = i.Latitude,
-            //         Longitude = i.Longitude,
-            //         Address = i.DeliveryAddress + " - "+ i.DateEncoded.ToString("hh:mm tt")
-            //     }).ToList();
-
-            // var shopLocationList = db.Orders.Where(i => i.Status == 2 || i.Status == 3)
-            //     .Join(db.Shops, o => o.ShopId, s => s.Id, (o, s) => new { o, s })
-            //     .Select(i => new
-            //     {
-            //         Latitude = i.s.Latitude,
-            //         Longitude = i.s.Longitude,
-            //         Address = i.s.Name + "-" + i.s.Address
-            //     }).ToList();
-
-            // var locationList = db.Orders.Where(i => i.Status == 2 || i.Status == 3)
-            //     .Join(db.Shops, o => o.ShopId, s => s.Id, (o, s) => new { o, s })
-            //     .Select(i => new
-            //     {
-            //         ShopLatitude = i.s.Latitude,
-            //         ShopLongitude = i.s.Longitude,
-            //         CustomerLatitude = i.o.Latitude,
-            //         CustomerLongitude = i.o.Longitude
-            //     }).ToList();
-
-            // return Json(new { joyralocation = joyraLocation,deliverylocation=deliverylocationlist,shopLocation=shopLocationList,locationlist = locationList }, JsonRequestBehavior.AllowGet);
         }
 
         //public void AddPaymentData(string code, int ordernumber)
