c1cea1e7eadb9119ee1f30e685b3d37a7414c9f3
developer1@joyra.in <developer1@joyra.in>
2022-02-16 15:56:35 +0530

    update

--- ShopNow/Api/ApiController.cs
+++ ShopNow/Api/ApiController.cs
@@ -5292,7 +5292,8 @@ namespace ShopNow.Controllers
         public JsonResult GetLiveOrderCount(int customerid)
         {
             var liveOrdercount = db.Orders.Where(i => i.CustomerId == customerid && i.Status != 0 && i.Status != 6 && i.Status != 7 && i.Status != 9 && i.Status != 10 && i.Status != -1).Count();
-            return Json(new { count = liveOrdercount }, JsonRequestBehavior.AllowGet);
+            var oldOrdercount = db.Orders.Where(i => i.CustomerId == customerid && i.Status == 6).Count();
+            return Json(new { count = liveOrdercount,oldOrderCount = oldOrdercount }, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult GetCheckOldCart(OldCartCheckViewModel model)
@@ -6109,17 +6110,21 @@ namespace ShopNow.Controllers
 
         public JsonResult AddCustomerSearchHistory(int customerid,int searchid,string serachtext,int type)
         {
-            var customerSH = new CustomerSearchHistory
+            var customerSearchHistory = db.CustomerSearchHistories.Any(i => i.CustomerId == customerid && i.SearchId == searchid && i.Status == 0);
+            if (!customerSearchHistory)
             {
-                CustomerId = customerid,
-                DateEncoded = DateTime.Now,
-                SearchId = searchid,
-                SearchText = serachtext,
-                Status = 0,
-                Type = type // 1- Product, 2-Shop
-            };
-            db.CustomerSearchHistories.Add(customerSH);
-            db.SaveChanges();
+                var customerSH = new CustomerSearchHistory
+                {
+                    CustomerId = customerid,
+                    DateEncoded = DateTime.Now,
+                    SearchId = searchid,
+                    SearchText = serachtext,
+                    Status = 0,
+                    Type = type // 1- Product, 2-Shop
+                };
+                db.CustomerSearchHistories.Add(customerSH);
+                db.SaveChanges();
+            }
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
