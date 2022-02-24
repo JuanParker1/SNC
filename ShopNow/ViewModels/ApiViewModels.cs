4e6ce9e5cfce9f52f2f8b3555411eb9e913bea59
developer1@joyra.in <developer1@joyra.in>
2022-02-24 14:04:31 +0530

    update

--- ShopNow/ViewModels/ApiViewModels.cs
+++ ShopNow/ViewModels/ApiViewModels.cs
@@ -951,7 +951,7 @@ namespace ShopNow.ViewModels
             public int Status { get; set; }
             public DateTime DateEncoded { get; set; }
             public int OrderNumber { get; set; }
-            public string OrderStatusText
+            public string StatusText
             {
                 get
                 {
@@ -980,7 +980,35 @@ namespace ShopNow.ViewModels
                     }
                 }
             }
-
+            public string StatusTextColor
+            {
+                get
+                {
+                    switch (this.Status)
+                    {
+                        case 2:
+                            return "text-primary";
+                        case 3:
+                            return "text-primary";
+                        case 4:
+                            return "text-warning";
+                        case 5:
+                            return "text-warning";
+                        case 6:
+                            return "text-success";
+                        case 7:
+                            return "text-danger";
+                        case 8:
+                            return "text-warning";
+                        case 9:
+                            return "text-danger";
+                        case 10:
+                            return "text-danger";
+                        default:
+                            return "text-danger";
+                    }
+                }
+            }
         }
     }
 }
