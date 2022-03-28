8ff7ac7f307ccbcbd7bc0ce9988fce374ab9395c
developer1@joyra.in <developer1@joyra.in>
2022-03-28 10:05:45 +0530

    update

--- ShopNow/Api/ApiController.cs
+++ ShopNow/Api/ApiController.cs
@@ -951,8 +951,8 @@ namespace ShopNow.Controllers
                     StringBuilder sb = new StringBuilder();
                     foreach (var item in model.ListItems)
                     {
-                        stock = Math.Floor(GetStockQty(item.ItemId.ToString(), item.OutletId)); //for gofrugal itemid id string
-                        //stock = Math.Floor(GetStockQty(Convert.ToInt32(item.ItemId), item.OutletId));
+                        //stock = Math.Floor(GetStockQty(item.ItemId.ToString(), item.OutletId)); //for gofrugal itemid id string
+                        stock = Math.Floor(GetStockQty(Convert.ToInt32(item.ItemId), item.OutletId));
                         if (stock < item.Quantity)
                         {
                             if (stock != 0)
@@ -3517,39 +3517,39 @@ namespace ShopNow.Controllers
             return Json(new { Page = "" }, JsonRequestBehavior.AllowGet);
         }
 
-        public static double GetStockQty(string code, int outletid) //for gofrugal itemid id string
+        //public static double GetStockQty(string code, int outletid) //for gofrugal itemid id string
+        //{
+        public double GetStockQty(int itemid, int outletid)
         {
-            //public double GetStockQty(int itemid, int outletid)
+
+            //using (WebClient myData = new WebClient())
             //{
 
-            using (WebClient myData = new WebClient())
-            {
+            //    myData.Headers["X-Auth-Token"] = "62AA1F4C9180EEE6E27B00D2F4F79E5FB89C18D693C2943EA171D54AC7BD4302BE3D88E679706F8C";
+            //    myData.Headers[HttpRequestHeader.Accept] = "application/json";
+            //    myData.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            //    string getList = "";
+            //    if (outletid == 0)
+            //        getList = myData.DownloadString("http://joyrahq.gofrugal.com/RayMedi_HQ/api/v1/items?q=status==R,outletId==2,locationId==1,itemId==" + code);
+            //    else
+            //        getList = myData.DownloadString("http://joyrahq.gofrugal.com/RayMedi_HQ/api/v1/items?q=status==R,locationId==1,outletId==" + outletid + ",itemId==" + code);
 
-                myData.Headers["X-Auth-Token"] = "62AA1F4C9180EEE6E27B00D2F4F79E5FB89C18D693C2943EA171D54AC7BD4302BE3D88E679706F8C";
-                myData.Headers[HttpRequestHeader.Accept] = "application/json";
-                myData.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-                string getList = "";
-                if (outletid == 0)
-                    getList = myData.DownloadString("http://joyrahq.gofrugal.com/RayMedi_HQ/api/v1/items?q=status==R,outletId==2,locationId==1,itemId==" + code);
-                else
-                    getList = myData.DownloadString("http://joyrahq.gofrugal.com/RayMedi_HQ/api/v1/items?q=status==R,locationId==1,outletId==" + outletid + ",itemId==" + code);
+            //    var result = JsonConvert.DeserializeObject<RootObject>(getList);
+            //    foreach (var pro in result.items)
+            //    {
+            //        foreach (var med in pro.stock)
+            //        {
+            //            return Convert.ToDouble(med.stock);
 
-                var result = JsonConvert.DeserializeObject<RootObject>(getList);
-                foreach (var pro in result.items)
-                {
-                    foreach (var med in pro.stock)
-                    {
-                        return Convert.ToDouble(med.stock);
-
-                    }
-                }
-            }
-
-            //var product = db.Products.FirstOrDefault(i => i.ItemId == itemid && i.OutletId == outletid);
-            //if (product != null)
-            //{
-            //    return product.Qty;
+            //        }
+            //    }
             //}
+
+            var product = db.Products.FirstOrDefault(i => i.ItemId == itemid && i.OutletId == outletid);
+            if (product != null)
+            {
+                return product.Qty;
+            }
             return 0;
         }
 
