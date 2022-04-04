0c866278016bdc8cc2414cbc593c683ecac89d7a
developer1@joyra.in <developer1@joyra.in>
2022-04-04 16:46:25 +0530

    update

--- ShopNow/ViewModels/PushNotificationViewModels.cs
+++ ShopNow/ViewModels/PushNotificationViewModels.cs
@@ -16,4 +16,18 @@ namespace ShopNow.ViewModels
             public string DistrictName { get; set; }
         }
     }
+
+    public class PushNotificationListViewModel
+    {
+        public List<ListItem> ListItems { get; set; }
+        public class ListItem
+        {
+            public int Index { get; set; }
+            public string DistrictName { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string EncodedBy { get; set; }
+            public DateTime DateEncoded { get; set; }
+        }
+    }
 }
